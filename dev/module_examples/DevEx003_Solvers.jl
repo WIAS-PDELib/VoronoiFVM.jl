1bde3b08d2152f7b59dba0808421b141f0c5cb9a
Documenter.jl <documenter@juliadocs.github.io>
2026-01-06 17:52:38 +0000

    build based on d1fc47d

--- dev/module_examples/DevEx003_Solvers.jl
+++ dev/module_examples/DevEx003_Solvers.jl
@@ -13,7 +13,7 @@ using ExtendableGrids
 using GridVisualize
 using LinearSolve
 using ExtendableSparse
-using ExtendableSparse: ILUZeroPreconBuilder, JacobiPreconBuilder, SmoothedAggregationPreconBuilder
+using ExtendableSparse: ILUZeroPreconBuilder, JacobiPreconBuilder
 using SparseArrays
 using AMGCLWrap
 using AlgebraicMultigrid
