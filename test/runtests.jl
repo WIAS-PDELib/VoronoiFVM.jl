60d8b0f18d20fd8d81f17af462f16bd84f245a58
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2026-04-21 22:59:21 +0200

    re-establish full tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,2 @@
 using Test
-#include("alltests.jl")
-
-using ExampleJuggler: ExampleJuggler, @testmodules
-ExampleJuggler.verbose!(true)
-@testmodules(joinpath(@__DIR__, "..", "examples"), ["Example440_ParallelState.jl"])
+include("alltests.jl")
