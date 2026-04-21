87fe7afa27a425bee1a71f4e6b115d05d095812b
Marieke Osewold <osewold@wias-berlin.de>
2026-04-21 15:46:47 +0200

    Rename tlinsolve_asm to tlinsolve_setup

--- src/vfvm_linsolve.jl
+++ src/vfvm_linsolve.jl
@@ -4,8 +4,8 @@ canonical_matrix(A) = A
 canonical_matrix(A::AbstractExtendableSparseMatrixCSC) = SparseMatrixCSC(A)
 
 function _solve_linear!(u, state, nlhistory, control, method_linear, A, b, reuse_precs)
-    tasm_1 = 0.0
-    tasm_2 = 0.0
+    tsetup_1 = 0.0
+    tsetup_2 = 0.0
     tsolve = 0.0
 
     if isnothing(state.linear_cache)
@@ -15,7 +15,7 @@ function _solve_linear!(u, state, nlhistory, control, method_linear, A, b, reuse
         Pl = nothing
         nlhistory.nlu += 1
         p = LinearProblem(canonical_matrix(A), b)
-        tasm_1 = @elapsed begin
+        tsetup_1 = @elapsed begin
             state.linear_cache = init(
                 p,
                 method_linear;
@@ -27,18 +27,18 @@ function _solve_linear!(u, state, nlhistory, control, method_linear, A, b, reuse
             )
         end
         if control.log
-            nlhistory.tlinsolve_asm += tasm_1
+            nlhistory.tlinsolve_setup += tsetup_1
         end
         if doprint(control, 'l')
             out = @sprintf("    [l]inear: factorize #%d\n", nlhistory.nlu)
             _info(out)
         end
     else
-        tasm_2 = @elapsed begin
+        tsetup_2 = @elapsed begin
             reinit!(state.linear_cache; A = canonical_matrix(A), b, reuse_precs)
         end
         if control.log
-            nlhistory.tlinsolve_asm += tasm_2
+            nlhistory.tlinsolve_setup += tsetup_2
         end
         if !reuse_precs
             nlhistory.nlu += 1
